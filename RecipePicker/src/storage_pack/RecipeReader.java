5b7793c79daa69578ede3f61a72698a6200e5801
Ajakaiye001 <99338208+Ajakaiye001@users.noreply.github.com>
2024-12-11 13:09:26 +0100

    Recipe to JSon file
    Connecting the regional Recipes to the Json file

--- RecipePicker/src/storage_pack/RecipeReader.java
+++ RecipePicker/src/storage_pack/RecipeReader.java
@@ -5,6 +5,9 @@ import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 import recipe_app.Recipe;
 import recipe_app.Region;
 import recipes.AfricanRecipe;
@@ -13,6 +16,57 @@ import recipes.WesternEuropeanRecipe;
 
 public class RecipeReader {
 	
+	private ArrayList<Recipe> recipeList = new ArrayList<>();
 	
+	 public RecipeReader(){
+	        String jsonFilePath = "recipes.json";
 
-}
+	        Gson gson = new Gson();
+
+	        try {
+	            Type africanRecipeListType = new TypeToken<List<AfricanRecipe>>() {}.getType();
+	            List<AfricanRecipe> africanRecipes = gson.fromJson(new FileReader(jsonFilePath), africanRecipeListType);
+
+	            // Print African Recipes
+	            for (AfricanRecipe recipe : africanRecipes) {
+	            	//System.out.println(recipe.getRegion());
+	            	if(recipe.getRegion() == Region.AFRICAN) recipeList.add(recipe);
+	            }
+
+	            
+	            Type SARecipeListType = new TypeToken<List<SouthAmericanRecipe>>() {}.getType();
+	            List<SouthAmericanRecipe> brazilianRecipes = gson.fromJson(new FileReader(jsonFilePath), SARecipeListType);
+
+	            for (SouthAmericanRecipe recipe : brazilianRecipes) {
+	            	if(recipe.getRegion() == Region.SOUTH_AMERICAN) recipeList.add(recipe);
+	            }
+
+	            // Load Irish Recipes
+	            Type WERecipeListType = new TypeToken<List<WesternEuropeanRecipe>>() {}.getType();
+	            List<WesternEuropeanRecipe> irishRecipes = gson.fromJson(new FileReader(jsonFilePath), WERecipeListType);
+
+	            // Print Irish Recipes
+	            for (WesternEuropeanRecipe recipe : irishRecipes) {
+	            	if(recipe.getRegion() == Region.WESTERN_EUROPE) recipeList.add(recipe);
+	            }
+	            
+
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+	        
+	        
+	        
+	    }
+
+		public ArrayList<Recipe> getRecipeList() {
+			return recipeList;
+		}
+
+		public void setRecipeList(ArrayList<Recipe> recipeList) {
+			this.recipeList = recipeList;
+		}
+	}
+
+
+
